52f85cc37f8e43f66d1f3177b26ce5f1c55e3363
supipi <rsupipi@gmail.com>
2020-02-22 23:30:03 +0530

    initial commit

---
+++ src/main/java/com/pipi/myMicroServices/MyMicroServicesApplication.java
@@ -0,0 +1,13 @@
+package com.pipi.myMicroServices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyMicroServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyMicroServicesApplication.class, args);
+	}
+
+}
